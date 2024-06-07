c625ec41892dd3068a0aa677605fc548fa4a8e94
Robson Diego Andrade <Robindiegoa@gmail.com>
2024-06-06 21:19:52 -0300

    :sparkles: feat: getReport endpoint done

--- src/controllers/agendamentoController.ts
+++ src/controllers/agendamentoController.ts
@@ -54,3 +54,53 @@ export const createAgendamento = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Erro ao realizar agendamento" });
     }
 }
+
+export const getAgendamentosQuantity = async (req: Request, res: Response) => {
+    try {
+
+        const { data_inicial, data_final } = req.body;
+
+        if (!data_inicial || !data_final) {
+            return res.status(400).json({ message: "Informe a data inicial e final" });
+        }
+
+        const initialDate = new Date(data_inicial.split("/").reverse().join("-"));
+        const finalDate = new Date(data_final.split("/").reverse().join("-"));
+
+
+        const schedulingQuantityPerBloodType = {
+            "A+": 0,
+            "A-": 0,
+            "B+": 0,
+            "B-": 0,
+            "AB+": 0,
+            "AB-": 0,
+            "O+": 0,
+            "O-": 0
+        }
+
+        const agendamentos = await AgendamentoModel.find({
+            data_doacao: {
+                $gte: initialDate.toISOString(),
+                $lte: finalDate.toISOString()
+            }
+        });
+
+        if (!agendamentos) {
+            return res.status(404).json({ message: "Nenhum agendamento encontrado" });
+        }
+
+        agendamentos.forEach(agendamento => {
+            const bloodType = agendamento.tipo_sanguineo as keyof typeof schedulingQuantityPerBloodType;
+            if (bloodType in schedulingQuantityPerBloodType) {
+                schedulingQuantityPerBloodType[bloodType]++;
+            }
+        });
+
+        return res.status(200).json(schedulingQuantityPerBloodType);
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: "Erro ao buscar agendamentos" });
+    }
+}
--- src/routes/agendamento.ts
+++ src/routes/agendamento.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createAgendamento } from "../controllers/agendamentoController";
+import { createAgendamento, getAgendamentosQuantity } from "../controllers/agendamentoController";
 
 const router = Router()
 
 router.post("/create/:usuarioId", createAgendamento)
+router.post("/getReport", getAgendamentosQuantity)
 
 export default router
